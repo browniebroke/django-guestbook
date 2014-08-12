ecd1994e92136ec70428325c5952575495ed9776
Browniebroke <alla.brunoo@gmail.com>
2014-08-12 21:28:57 +0100

    Remove a wrongly committed view

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -5,13 +5,12 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-from views import post_entry, EntryListView, api_list_entries, api_post_entry
+from views import post_entry, EntryListView, api_list_entries
 
 urlpatterns = patterns('',
     url(r'^$', EntryListView.as_view() , name='guestbook_page_last'),
     url(r'^page(?P<page>[0-9]+)/$', EntryListView.as_view(), name='guestbook_page'),
     url(r'^post/', post_entry, name='guestbook_post'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'api/list/', api_list_entries, name='api_guestbook_list'),
-    url(r'api/post/', api_post_entry, name='api_guestbook_post'),
+    url(r'api/list/', api_list_entries, name='api_guestbook_list')
 )
